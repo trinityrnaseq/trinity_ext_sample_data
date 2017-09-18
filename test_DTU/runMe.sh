8e59aea16703a7c9f3129f4c115e181565c88bf8
Brian Haas <brianjohnhaas@gmail.com>
2017-09-18 13:00:58 -0400

    add plotter

--- test_DTU/runMe.sh
+++ test_DTU/runMe.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ../../Analysis/SuperTranscripts/DTU/dexseq_wrapper.pl --genes_fasta minigenome.fa --genes_gtf minigenome.gtf --samples_file samples.txt --out_prefix G
 
 rm -f ./*.ok
@@ -7,3 +9,6 @@ rm -f ./*.ok
 ../../Analysis/SuperTranscripts/DTU/dexseq_wrapper.pl --genes_fasta trinSuper.fasta --genes_gtf trinSuper.gtf --samples_file samples.txt --out_prefix S
 
 ./compare_dexseq_results.pl G.results.dat S.results.dat > compare.dat
+
+./plot_comparison.Rscript
+
