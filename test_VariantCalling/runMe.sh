082dd165182025641c99d05afebfaf9fa88e4e88
Brian Haas <brianjohnhaas@gmail.com>
2017-09-19 20:06:33 -0400

    update

--- test_VariantCalling/runMe.sh
+++ test_VariantCalling/runMe.sh
@@ -2,6 +2,10 @@
 
 set -ev
 
-${TRINITY_HOME}/Analysis/SuperTranscripts/AllelicVariants/run_variant_calling.py --st supertranscripts.fasta -p whitefly_rnaseq_1.fq.gz whitefly_rnaseq_2.fq.gz -o test_varcalling
+${TRINITY_HOME}/Analysis/SuperTranscripts/AllelicVariants/run_variant_calling.py \
+               --st_fa supertranscripts.fasta \
+               --st_gtf supertranscripts.gtf \
+               -p whitefly_rnaseq_1.fq.gz whitefly_rnaseq_2.fq.gz \
+               -o test_varcalling
 
 
