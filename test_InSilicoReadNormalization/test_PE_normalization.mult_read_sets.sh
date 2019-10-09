eed18452e19e52b7fb58454ad271575208205dd6
brianjohnhaas <brianjohnhaas@gmail.com>
2019-10-09 15:47:22 +0000

    updates for docker compatibility

--- test_InSilicoReadNormalization/test_PE_normalization.mult_read_sets.sh
+++ test_InSilicoReadNormalization/test_PE_normalization.mult_read_sets.sh
@@ -2,8 +2,8 @@
 
 # just for testing purposes, use --max_cov 30 or higher for real applications.
 $TRINITY_HOME/util/insilico_read_normalization.pl --JM 2G \
-     --left ../test_Trinity_Assembly/reads.left.fq.gz,../test_Trinity_Assembly/reads2.left.fq.gz \
-     --right ../test_Trinity_Assembly/reads.right.fq.gz,../test_Trinity_Assembly/reads2.right.fq.gz \
+     --left ../test_DATA/reads.left.fq.gz,../test_DATA/reads2.left.fq.gz \
+     --right ../test_DATA/reads.right.fq.gz,../test_DATA/reads2.right.fq.gz \
      --seqType fq --max_cov 5 --pairs_together --no_cleanup --CPU 4 --tmp_dir_name tmp_PE_mult_norm_dir \
      --output test_multi_read_sets_norm_outdir
 
--- test_InSilicoReadNormalization/test_PE_normalization.w_base_cov_stats.sh
+++ test_InSilicoReadNormalization/test_PE_normalization.w_base_cov_stats.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -ve
 
 if [ ! -e reads.left.fq ]; then
-    gunzip -c ../test_Trinity_Assembly/reads.left.fq.gz > reads.left.fq
+    gunzip -c ../test_DATA/reads.left.fq.gz > reads.left.fq
 fi
 
 if [ ! -e reads.right.fq ]; then
-    gunzip -c ../test_Trinity_Assembly/reads.right.fq.gz > reads.right.fq
+    gunzip -c ../test_DATA/reads.right.fq.gz > reads.right.fq
 fi
 
 # just for testing purposes, use --max_cov 30 or higher for real applications.
-../../util/insilico_read_normalization.pl --JM 2G --left reads.left.fq --right reads.right.fq --seqType fq --max_cov 5 --pairs_together --__devel_report_kmer_cov_stats
+$TRINITY_HOME/util/insilico_read_normalization.pl --JM 2G --left reads.left.fq --right reads.right.fq --seqType fq --max_cov 5 --pairs_together --__devel_report_kmer_cov_stats
+
+
+
+
 
 
 
