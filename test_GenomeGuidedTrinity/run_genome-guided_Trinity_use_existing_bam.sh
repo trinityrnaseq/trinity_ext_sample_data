efaa14c2c03d6a7ec83f39d0fc88c3a010339e79
Brian Haas <brianjohnhaas@gmail.com>
2021-09-03 13:11:17 +0000

    add test coverage for genome-guided assembly output

--- test_GenomeGuidedTrinity/run_genome-guided_Trinity_use_existing_bam.sh
+++ test_GenomeGuidedTrinity/run_genome-guided_Trinity_use_existing_bam.sh
@@ -20,3 +20,13 @@ fi
 
 
 $TRINITY_HOME/Trinity --genome_guided_max_intron 1000 --genome_guided_bam SP2.chr.bam --max_memory 2G  --output test_GG_use_bam_trinity_outdir
+
+asmsize=$(wc -c <"test_GG_use_bam_trinity_outdir/Trinity-GG.fasta")
+if [ $asmsize -le 50000 ]; then
+    echo "Error, test_GG_use_bam_trinity_outdir/Trinity-GG.fasta potentially incomplete"
+    exit 1
+else
+    echo "ok"
+    exit 0
+fi
+
--- test_GenomeGuidedTrinity/run_small_GG_mutliScaff_test.sh
+++ test_GenomeGuidedTrinity/run_small_GG_mutliScaff_test.sh
@@ -2,3 +2,14 @@
 
 
 $TRINITY_HOME/Trinity --genome_guided_max_intron 1000 --genome_guided_bam transAligns.cSorted.bam --max_memory 2G  --output test_GG_use_small_multiscaff_bam_trinity_outdir
+
+
+outfile="test_GG_use_small_multiscaff_bam_trinity_outdir/Trinity-GG.fasta"
+asmsize=$(wc -c <"${outfile}")
+if [ $asmsize -le 650000 ]; then
+    echo "Error, ${outfile} potentially incomplete"
+    exit 1
+else
+    echo "ok"
+    exit 0
+fi
