479f5b4f2432cf48379a89bed1fc8da1dc667aa9
Michael R. Crusoe <crusoe@debian.org>
2021-11-08 11:29:14 +0100

    Enable better use of sub-make

--- test_align_and_estimate_abundance/SINGLE_END_ABUNDANCE_ESTIMATION/misc_tests/test_Mouse_ref.sh
+++ test_align_and_estimate_abundance/SINGLE_END_ABUNDANCE_ESTIMATION/misc_tests/test_Mouse_ref.sh
@@ -1,1 +1,2 @@
-make TRINITY_FASTA=/seq/regev_genome_portal/RESOURCES/mouse/mm9/Annotations/mm9_ucsc_genomeStudio_genes/mm9_ucsc_genomeStudio_genes.gtf.trans.fa SAMPLES=misc_tests/mouse_ref.samples.txt
+MAKE=${MAKE:-make}
+${MAKE} TRINITY_FASTA=/seq/regev_genome_portal/RESOURCES/mouse/mm9/Annotations/mm9_ucsc_genomeStudio_genes/mm9_ucsc_genomeStudio_genes.gtf.trans.fa SAMPLES=misc_tests/mouse_ref.samples.txt
